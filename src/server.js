4f285f73862abb0e611b531b028bc61be3259e41
Yozzy Lazzy <kristian_yosef@yahoo.com>
2023-07-01 00:15:48 +0700

    Update package and eslint

--- src/server.js
+++ src/server.js
@@ -1,19 +1,13 @@
 const Hapi = require('@hapi/hapi');
 
 const init = async () => {
-	const server = Hapi.server({
-		port: 3000,
-		host: 'localhost'
-	});
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+  });
 
-	server.route({
-		method: 'GET',
-		path: '/',
-		handler: (request, h) => {
-			return 'Hello World!';
-		}
-	});
-
-	await server.start();
-	console.log('Server running on %s', server.info.uri);
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
+
+init();

20534fc1b917df68153db66d54889e24a63f470b
Yozzy Lazzy <kristian_yosef@yahoo.com>
2023-07-02 21:12:43 +0700

    Update Add postgres and some files

--- src/server.js
+++ src/server.js
@@ -1,4 +1,13 @@
+// eslint-disable-next-line quotes
+require(`dotenv`).config();
+
 const Hapi = require('@hapi/hapi');
+const albums = require('./api/albums');
+const songs = require('./api/songs');
+const AlbumsService = require('./services/postgres/AlbumsService');
+const SongsService = require('./services/postgres/SongsService');
+const AlbumsValidator = require('./validator/albums');
+const SongsValidator = require('./validator/songs');
 
 const init = async () => {
   const server = Hapi.server({

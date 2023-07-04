a7934f7681f42cf6225c898d5d46a96c5468c6f1
Yozzy Lazzy <kristian_yosef@yahoo.com>
2023-07-05 01:05:13 +0700

    Masih error

--- src/services/postgres/AlbumsService.js
+++ src/services/postgres/AlbumsService.js
@@ -29,6 +29,7 @@ class AlbumsService {
       text: 'SELECT * FROM albums WHERE id = $1',
       values: [id],
     };
+
     const result = await this._pool.query(query);
     if (!result.rows.length) {
       throw new NotFoundError('Album tidak ditemukan');

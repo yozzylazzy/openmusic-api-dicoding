070818359141f41be285bace635158220127ca15
Yozzy Lazzy <kristian_yosef@yahoo.com>
2023-07-27 20:57:25 +0700

    update schema validaton Joi

--- src/validator/albums/schema.js
+++ src/validator/albums/schema.js
@@ -1,7 +1,7 @@
 const Joi = require('joi');
 
 const AlbumPayloadSchema = Joi.object({
-  name: Joi.string().required(),
+  name: Joi.string().max(50).required(),
   year: Joi.number().integer().min(1900).max(new Date().getFullYear())
     .required(),
 });
